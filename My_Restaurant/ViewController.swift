34ed93597ec0ef2751b6ccf2e117eeb856c53bfa
Dmitry Parkhomenko <oku_74@mail.ru>
2024-01-23 16:01:15 +0500

    Initial Commit

---
+++ My_Restaurant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My_Restaurant
+//
+//  Created by Dmitry Parhomenko on 23.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
